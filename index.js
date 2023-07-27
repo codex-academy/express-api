062c251edb2c4ae8e9e59065da61f63239efdba6
avermeulen <andre.vermeulen@gmail.com>
2023-07-27 13:14:17 +0200

    added PORT config

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ app.get('/api/greet/:username', function(req, res){
     })
 });
 
-const PORT = 4009
+const PORT = process.env.PORT || 4009;
 
 app.listen(PORT, function(){
     console.log(`app started on port ${PORT}`)
